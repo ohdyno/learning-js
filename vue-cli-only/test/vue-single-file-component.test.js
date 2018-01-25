603e02478ea4d4ace1a3209ae9fedea027588d92
Xing Zhou <xzhou@pivotal.io>
2018-01-25 04:51:25 -0500

    Disable Vue production tip during test
    This is done to reduce unnecessary noise.

--- vue-cli-only/test/vue-single-file-component.test.js
+++ vue-cli-only/test/vue-single-file-component.test.js
@@ -2,4 +2,6 @@ import Fizzbuzz from '../main/Fizzbuzz.vue'
 import Contract from '../../test/contract.test';
 import Vue from "vue";
 
+Vue.config.productionTip = false;
+
 new Contract(Vue.extend(Fizzbuzz)).run();
